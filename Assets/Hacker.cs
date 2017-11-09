d46edd80b35bea73759e90a65d49aaa4a9b49342
Kyle Chadwick <kylepchad@gmail.com>
2017-11-08 20:59:52 -0800

    level 1 and 2 passwords

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game configuration data
+    string[] level1Passwords = { "heart", "brain", "bone", "spine", "blood" };
+    string[] level2Passwords = { "muscle", "appendix", "pancreas", "injection", "anesthetic" };
+
     // Game state
-    int level; 
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+    string password;
 
     // Use this for initialization
     void Start()
@@ -16,6 +23,7 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu(string greeting)
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine(greeting);
         Terminal.WriteLine("");
@@ -33,27 +41,65 @@ public class Hacker : MonoBehaviour
         if (input == "menu")
         {
             ShowMainMenu("Let's try again");
+            currentScreen = Screen.MainMenu;
         }
-        else if (input == "1"){
+        else if (currentScreen == Screen.MainMenu){
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password){
+            CheckPassword(input);
+        }
+        else if (currentScreen == Screen.Win){
+            
+        }
+        else {
+            
+        }
+    }
+
+
+
+    void RunMainMenu(string input) {
+        if (input == "1"){
             level = 1;
+            password = level1Passwords[2]; // to do: make random
             StartGame();
         }
-        else if (input == "2")
-        {
+        else if (input == "2"){
             level = 2;
+            password = level2Passwords[1]; // to do: make random
             StartGame();
         }
         else if (input == "bandaid"){
             Terminal.WriteLine("bandaids won't fix everything, kid.");
             Terminal.WriteLine("Go to med school.");
         }
-        else {
+        else{
             Terminal.WriteLine("invalid response");
         }
     }
 
-    void StartGame()
-    {
+    void StartGame() {
+        currentScreen = Screen.Password;
         Terminal.WriteLine("You have selected level " + level);
+        if (level == 1) {
+            Terminal.WriteLine("enter password:");
+        }
+        else if (level == 2){
+            Terminal.WriteLine("enter password:");
+        }
+        else {
+            
+        }
     }
+
+    void CheckPassword(string input) {
+        if (input == password) {
+            Terminal.WriteLine("You Win!");
+        }
+        else {
+            Terminal.WriteLine("Try again...");
+        }
+    }
+
 }
